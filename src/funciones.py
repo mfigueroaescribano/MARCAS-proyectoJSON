971fb10e828c19a23e3068177eb632550bb82aaa
Miguel Figueroa Escribano <miguelfigueroaescribano@gmail.com>
2023-02-28 17:10:09 +0100

    Funciones 1-4

--- src/funciones.py
+++ src/funciones.py
@@ -1,3 +1,48 @@
+import json
+
+with open("data/amazon.books.json","r") as f:
+    listaLibros = json.load(f)
+
 def listaTitulosLibros(listaLibros):
-    for book in listaLibros:
-        print(book["title"])
+    for libro in listaLibros["books"]:
+        print(libro["title"])
+
+def numAutores(listaLibros):
+    listaAutores = []
+    for libro in listaLibros["books"]:
+        for autor in libro["authors"]:
+            if autor not in listaAutores:
+                listaAutores.append(autor)
+    
+    print(f"Hay un total de {len(listaAutores)} autores en todo el catálogo")
+    opcionAutores = input("¿Quieres listar todos los autores? [Y/N]: ")
+    if opcionAutores == "Y":
+        for autor in listaAutores:
+            print(autor)
+
+def busquedaTituloLimite(listaLibros):
+    aux = []
+    textoBusqueda = input("Introduzca el texto del título para buscar: ")
+    limitePaginas = int(input("Introduzca el límite de páginas: "))
+    for libro in listaLibros["books"]:
+        if textoBusqueda in libro["title"] and libro["pageCount"] < limitePaginas:
+            aux.append(libro["title"])
+    for libro in aux:
+        print(libro)
+
+def librosPorAutor(listaLibros):
+    aux = []
+    autorBusqueda = input("Introduzca el autor para obtener sus libros: ")
+    for libro in listaLibros["books"]:
+        for autor in libro["authors"]:
+            if autorBusqueda == autor:
+                aux.append(libro["title"])
+    print("\nLos libros escritos por el autor son: ")
+    for libro in aux:
+        print(libro)
+
+def actualizacionPorISBN(listaLibros):
+    isbnBusqueda = input("Introduzca el ISBN del libro a buscar: ")
+    for libro in listaLibros["books"]:
+        if isbnBusqueda in libro["isbn"]:
+                print("Hemos encontrado el siguiente libro: ", libro["title"])
--- src/programa.py
+++ src/programa.py
@@ -1,8 +1,33 @@
-import json
 from funciones import *
 
-with open("data/amazon.books.json","r") as f:
-    datos = json.load(f)
+print("Bienvenido al proyecto JSON")
 
-print(datos)
-# listaTitulosLibros(data)
+menu='''
+    1 - Lista los títulos de los libros
+    2 - Número de autores
+    3 - Búsqueda por título y límite de páginas
+    4 - Libros escritos por autor
+    5 - Modificación de datos del libro
+    6 - Salir
+    '''
+
+opcion = 0
+
+while opcion !=6:
+    print(menu)
+    opcion = int(input("Introduce una opción: "))
+
+    if opcion==1:
+        listaTitulosLibros(listaLibros)
+
+    if opcion==2:
+        numAutores(listaLibros)
+
+    if opcion==3:
+        busquedaTituloLimite(listaLibros)
+
+    if opcion==4:
+        librosPorAutor(listaLibros)
+
+    if opcion==5:
+        actualizacionPorISBN(listaLibros)
